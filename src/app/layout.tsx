e0936c103057139e2642166829fc8ec59541a668
Anish Kancherla <anish.kancherla@gmail.com>
2025-06-09 17:25:02 -0700

    layout change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Anish Kancherla - Developer & Designer",
+  title: "Anish Kancherla",
 };
 
 export default function RootLayout({
